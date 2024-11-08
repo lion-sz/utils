f37cd2b95b106e37c4fd8c85eabecd326053f8ce
Lion Szlagowski <lion.szlagowski@unisg.ch>
2024-11-08 13:39:02 +0000

    add disable-cache option

--- app_template/_typer_callback.py
+++ app_template/_typer_callback.py
@@ -3,13 +3,20 @@ import sys
 
 from app_template import ConfigReader
 
-def setup_callback(app):
 
-    @app.callback(["name", "config", "config_overwrite"])
-    def load_config(name: str | None = None, config: list[str] = ["./config"]):
+def setup_callback(app):
+    # @app.callback(["name", "config", "config_overwrite"])
+    @app.callback()
+    def load_config(
+        name: str | None = None,
+        config: list[str] = ["./config"],
+        disable_cache: bool = False,
+    ):
         if len(config) == 0:
             typer.echo("No config provided. Exiting...")
             sys.exit(1)
         config = ConfigReader(name).load(config[0])
+        if disable_cache:
+            config.use_cache = False
         sys.modules["app_template"].config = config
         return
