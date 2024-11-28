5e306cdc68bc6104ca5b195c250e456e8b578612
Lion Szlagowski <lion-sz@gmx.de>
2024-11-28 13:40:07 +0000

    respect args in cache

--- tools/_cached.py
+++ tools/_cached.py
@@ -1,3 +1,4 @@
+import hashlib
 from typing import Callable
 import sys
 import functools
@@ -5,16 +6,24 @@ from datetime import datetime, timedelta
 
 import polars as pl
 
-def cached(name: str, retention_hours: int = 0) -> Callable:
+def cached(name: str, retention_hours: int = 0, use_args: bool = False) -> Callable:
     """Cache a function result.
+
+    If the cache should find a fitting file based on the arguments as well,
+    the cache function will call `str` on all arguments,
+    combine the results and hash.
+    Please make sure that by doing so you can actually differentiate the results.
+
     Args:
         name (str): The name to use for the cache file.
         retention_hours (int): The number of hours that a file is cached for.
+        use_args (bool): Whether to consider the arguments when looking
+            for a viable cache file.
     """
-    return lambda f: _cached(f, name, retention_hours)
+    return lambda f: _cached(f, name, retention_hours, use_args)
 
 
-def _cached(func, name: str, retention_hours: int):
+def _cached(func, name: str, retention_hours: int, use_args: bool):
     @functools.wraps(func)
     def inner(*args, **kwargs):
         config = sys.modules["app_template"].config
@@ -22,12 +31,21 @@ def _cached(func, name: str, retention_hours: int):
             raise ValueError("Cannot use `cached` without `app`.")
         if not config.use_cache:
             return func(*args, **kwargs)
+        # Check the arguments
+        if use_args:
+            arg_s = [str(a) for a  in args]
+            for k, v in kwargs.items():
+                arg_s.append(k)
+                arg_s.append(str(v))
+            suffix = "_" + hashlib.md5("---".join(arg_s).encode()).hexdigest()
+        else:
+            suffix = ""
         # Check if a cached file exists.
         cache_path = config.cache_path
         if not cache_path.exists():
             cache_path.mkdir()
         cache_file = None
-        candidates = sorted(cache_path.glob(f"{name}_*.parquet"), reverse=True)
+        candidates = sorted(cache_path.glob(f"{name}{suffix}_*.parquet"), reverse=True)
         # Go through the candidates and filter out too old ones.
         retention = timedelta(hours=retention_hours)
         for cand in candidates:
@@ -40,7 +58,7 @@ def _cached(func, name: str, retention_hours: int):
                 cache_file = cand
         if cache_file is None:
             ts = int(datetime.now().timestamp())
-            cache_file = cache_path / f"{name}_{ts}.parquet"
+            cache_file = cache_path / f"{name}{suffix}_{ts}.parquet"
             res = func(*args, **kwargs)
             res.write_parquet(cache_file)
             return res
